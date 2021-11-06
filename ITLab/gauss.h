5b492417f88b90cd174bdf1f18249cddbfd30092
LarinK65 <konstantinlarin02@gmail.com>
2021-11-06 09:52:38 +0300

    Update gauss.h

--- ITLab/gauss.h
+++ ITLab/gauss.h
@@ -3,36 +3,8 @@
 #include <vector>
 #include <utility>
 
-
-template <typename U_M, typename M>
-M calc_U(const U_M& a, const M& b, size_t a_size, size_t b_columns)
-{
-	M ans(a_size, b_columns);
-
-	for (int64_t i = a_size - 1; i + 1 > 0; i--)
-	{
-	#pragma omp parallel for
-		for (int64_t j = 0; j < b_columns; j++)
-		{
-			for (size_t k = i + 1; k < a_size; k++)
-			{
-				ans[i][j] += a[i][k] * ans[k][j];
-			}
-
-			ans[i][j] = b[i][j] - ans[i][j];
-
-			ans[i][j] /= a[i][i];
-		}
-	}
-
-
-	return ans;
-}
-
-
-
 template <typename L_M, typename M, typename R_T>
-R_T calc_L(const L_M& a, const M& b, size_t a_size, size_t b_columns)
+R_T solve_lower_system_basic(const L_M& a, const M& b, size_t a_size, size_t b_columns)
 {
 	R_T ans(a_size, b_columns);
 
@@ -41,13 +13,14 @@ R_T calc_L(const L_M& a, const M& b, size_t a_size, size_t b_columns)
 	#pragma omp parallel for
 		for (int64_t j = 0; j < b_columns; j++)
 		{
+			auto sum = ans[i][j];
+		#pragma omp simd reduction(+: sum)
 			for (size_t k = 0; k < i; k++)
 			{
-				ans[i][j] += a[i][k] * ans[k][j];
+				sum += a[i][k] * ans[k][j];
 			}
 
-			ans[i][j] = b[i][j] - ans[i][j];
-
+			ans[i][j] = b[i][j] - sum;
 			ans[i][j] /= a[i][i];
 		}
 	}
@@ -65,33 +38,34 @@ R_T solve_lower_system_blocks(const L_M& a, const M& b, size_t a_size, size_t b_
 
 	using value_type = std::decay_t<decltype(a[0][0])>;
 
-	constexpr size_t block_size = 1024 / sizeof(value_type);
+	constexpr size_t block_size = 512 / sizeof(value_type);
 
 	for (size_t first_block = 0; first_block < a_size; first_block += block_size)
 	{
 	#pragma omp parallel for
-		for (int64_t second_block = 0; second_block < b_columns; second_block += block_size)
+		for (size_t second_block = 0; second_block < b_columns; second_block += block_size)
 		{
 			for (size_t i = first_block; i < a_size && i < first_block + block_size; i++)
 			{
 				size_t e = std::min(b_columns, second_block + block_size);
-				//#pragma omp parallel for
 				for (int64_t j = second_block; j < e; j++)
 				{
+					auto sum = ans[i][j];
+				#pragma omp simd reduction(+: sum)
 					for (size_t k = first_block; k < i; k++)
 					{
-						ans[i][j] += a[i][k] * ans[k][j];
+						sum += a[i][k] * ans[k][j];
 					}
 
-					ans[i][j] = b[i][j] - ans[i][j];
+					ans[i][j] = b[i][j] - sum;
 					ans[i][j] /= a[i][i];
 				}
 			}
 		}
 
+		#pragma omp parallel for
 		for (size_t nfirst_block = first_block + block_size; nfirst_block < a_size; nfirst_block += block_size)
 		{
-		#pragma omp parallel for
 			for (size_t second_block = 0; second_block < b_columns; second_block += block_size)
 			{
 				for (size_t i = nfirst_block; i < a_size && i < nfirst_block + block_size; i++)
@@ -100,52 +74,17 @@ R_T solve_lower_system_blocks(const L_M& a, const M& b, size_t a_size, size_t b_
 
 					for (size_t j = second_block; j < e; j++)
 					{
+						auto sum = ans[i][j];
+					#pragma omp simd reduction(+: sum)
 						for (size_t k = first_block; k < first_block + block_size; k++)
 						{
-							ans[i][j] += a[i][k] * ans[k][j];
+							sum += a[i][k] * ans[k][j];
 						}
+
+						ans[i][j] = sum;
 					}
 				}
 			}
-		}
-	}
-
-	return ans;
-}
-
-
-template <typename U_M, typename M>
-M solve_upper_system_blocks(const U_M& a, const M& b, size_t a_size, size_t b_columns)
-{
-	M ans(a_size, b_columns);
-
-	using value_type = std::decay_t<decltype(b[0][0])>;
-
-	constexpr size_t block_size = 2048 / sizeof(value_type);
-
-	for (size_t i = a_size - 1; i + 1 > 0; i--)
-	{
-		for (int64_t start_block = i + 1; start_block < a_size; start_block += block_size)
-		{
-		#pragma omp parallel for
-			for (int64_t j = 0; j < b_columns; j++)
-			{
-				value_type sum = 0;
-				for (int64_t k = start_block; k < a_size && k < start_block + block_size; k++)
-				{
-					sum += a[i][k] * ans[k][j];
-				}
-
-				ans[i][j] += sum;
-			}
-		}
-
-
-	#pragma omp parallel for
-		for (int64_t j = 0; j < b_columns; j++)
-		{
-			ans[i][j] = b[i][j] - ans[i][j];
-			ans[i][j] /= a[i][i];
 		}
 	}
 
